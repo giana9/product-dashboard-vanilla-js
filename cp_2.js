326ca9b1c3a145f28dac3fd154b7a0ab1b550cfd
Giana Cresta <crestagiana@gmail.com>
2026-06-28 20:04:19 -0400

    fetchProductsAsync()

--- cp_2.js
+++ cp_2.js
@@ -10,3 +10,14 @@ function fetchProductsThen() {
     .catch(error => {
       console.log("Fetch error:", error);
     });
+
+// fetchProductsAsync()
+async function fetchProductsAsync() {
+  try {
+    const response = await fetch("https://www.course-api.com/javascript-store-products");
+    const products = await response.json();
+    displayProducts(products);
+  } catch (error) {
+    handleError(error);
+  }
+}
